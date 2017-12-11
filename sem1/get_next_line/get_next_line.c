7cc75c7e9ba3f9b6edf8ff707a5c82bbb59cc8ab
Murray MACDONALD <mmacdona@e4r6p4.wethinkcode.co.za>
2017-12-11 18:52:19 +0200

    GNL!;

--- sem1/get_next_line/get_next_line.c
+++ sem1/get_next_line/get_next_line.c
@@ -38,24 +38,28 @@ int		make_line(char **line, t_list **head)
 		}
 		else
 			tempStr = current->content;
+// printf("tempStr: %s\n", tempStr);
 		*line = ft_strjoin(*line, tempStr);
-		//if (tempStr != current->content && tempStr != NULL)
-			//free(tempStr);
+		// if (tempStr != current->content && tempStr != NULL)
+		// 	free(tempStr);
+		// if (current->content != NULL)
+		// {
+		// 	free(current->content);
+		// 	current->content = NULL;
+		// }
 		current = current->next;
 	}
+	tempInt = count_size(head);
 	// while (*head != NULL)
 	// {
 	// 	current = *head;
-	// 	while (current->next != NULL)
-	// 		current = current->next;
-	// 	if (current->content != NULL)
-	// 		free(current->content);
-	// 	if (current != NULL)
-	// 		free(current);
+	// 	*head = current->next;
+	// 	free(current);
+	// 	current = NULL;
 	// }
 
-	(*line)[count_size(head)] = '\0';
-	return (count_size(head));
+	(*line)[tempInt] = '\0';
+	return (tempInt);
 }
 
 int		read_to_list(const int fd, t_list **head)
@@ -70,49 +74,51 @@ int		read_to_list(const int fd, t_list **head)
 
 	temp = ft_strnew(BUFF_SIZE + 1);
 	readStatus = 1;
-	if (save == NULL)
+// printf("Save: %s\n", save);
+	if (save != NULL && ft_strlen(save) > 0)
+	{
+// printf("Save: %s AND SaveLen:%zu\n", save, ft_strlen(save));
+		ft_strcpy(temp, save);
+		free(save);
+		save = NULL;
+	}
+	else
 	{
 		readStatus = read(fd, buff, BUFF_SIZE);
 //printf("post read- readStatus: %i\n", readStatus);
 		buff[readStatus] = '\0';
 		ft_strcpy(temp, (char *)&buff);
 	}
-	else
-	{
-		ft_strcpy(temp, save);
-		// if (save != NULL)
-		// 	free(save);
-		save = NULL;
-	}
 
 	if (readStatus <= 0)
 		return (readStatus);
 	
 	tempInt = ft_strsearch(temp, '\n');
-//printf("Temp: %s\nTempInt: %i\n Save:%s\n", temp, tempInt, save);
+// printf("Temp: %s\nTempInt: %i\n", temp, tempInt);
 	if (tempInt >= 0)
 	{
 		if (tempInt > 0)
 			new = ft_lstnew(temp, tempInt + 1);
 		if (tempInt != (BUFF_SIZE - 1) && tempInt != 0)
 		{
-			save = ft_strnew(BUFF_SIZE - tempInt + 1);
-			ft_strcpy(save, &temp[tempInt + 1]);
+			save = ft_strnew(BUFF_SIZE - tempInt);
+			ft_strcpy(save, &temp[tempInt]);
 		}
 		if (tempInt == 0)
 		{
 			save = ft_strnew(BUFF_SIZE - 1);
 			ft_strcpy(save, &temp[1]);
-			return (0);
+			return (-2);
 		}
-		readStatus = 0;
+		readStatus = -2;
+// printf("Returning -2\n");
 		//new line found and not at [0]
 	}
 	if (tempInt == -1)
 		new = ft_lstnew(temp, ft_strlen(temp));
 		//new line not found
-	// if (temp != NULL)
-	// 	free(temp);
+	if (temp != NULL)
+		free(temp);
 	ft_lstadd(head, new);
 	return (readStatus);
 }
@@ -131,10 +137,14 @@ int		get_next_line(const int fd, char **line)
 		readStatus = read_to_list(fd, &head);
 	}
 	makeStatus = make_line(line, &head);
-//printf("Makestatus: %i\n", makeStatus);
-	if (makeStatus > 0)
-		return (1);
+printf("Makestatus: %i\n", makeStatus);
+printf("ReadStatus: %i\n", readStatus);
+printf("Return 0\n");
 	if (readStatus == 0 && makeStatus == 0)
 		return (0);
+printf("Return 1\n");
+	if (readStatus == -2 || makeStatus > 0)
+		return (1);
+printf("Return -1\n");
 	return (-1);
 }
--- sem1/get_next_line/main.c
+++ sem1/get_next_line/main.c
@@ -8,6 +8,41 @@
 
 int     main()
 {
+	// char 	*line;
+	// int		out;
+	// int		p[2];
+	// int		fd;
+	// int		gnl_ret;
+
+	// out = dup(1);
+	// pipe(p);
+
+	// fd = 1;
+	// dup2(p[1], fd);
+	// write(fd, "abc\n\n", 5);
+	// close(p[1]);
+	// dup2(out, fd);
+
+	// /* Read abc and new line */
+	// gnl_ret = get_next_line(p[0], &line);
+	// printf("%d\n", gnl_ret);
+	// printf("%d\n", strcmp(line, "abc"));
+	// printf("%s\n", line);
+
+	//  Read new line 
+	// gnl_ret = get_next_line(p[0], &line);
+	// printf("%d\n", gnl_ret);
+	// printf(":%s:\n", line);
+
+	// /* Read again, but meet EOF */
+	// gnl_ret = get_next_line(p[0], &line);
+	// printf("%d\n", gnl_ret);
+	// printf(":%s:\n", line);
+
+	// /* Let's do it once again */
+	// gnl_ret = get_next_line(p[0], &line);
+	// printf("%d\n", gnl_ret);
+	// printf(":%s:\n", line);
 	int     fd;
 	char    *line;
 	int		i;
